5b86598567c86ced3a72a0d01d1828fc2ead30bb
Maksym Prymierov <primerovmax@gmail.com>
2019-08-28 18:00:06 +0300

    Add number print function to early_printk()
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

--- arch/x86/tty.c
+++ arch/x86/tty.c
@@ -37,9 +37,7 @@ int text_graphic_init(u16 *vid_buf)
 	video_buffer = vid_buf;
 
 	clear_screen();
-        char *s = "Hello, world";
 	early_printk("kernel: text graphic is initialized\n");
-        early_printk("String: %s\n", s);
 
 	return 0;
 
@@ -54,7 +52,6 @@ static void puts(const char* message)
                 switch (message[i]) {
                 case '\n':
                         cursor = (cursor / TEXT_GRAPHIC_WIDTH + 1) * TEXT_GRAPHIC_WIDTH;
-                        ++i;
                         break;
                 default:
                         video_buffer[cursor] = TEXT_GRAPHIC_COLOR + message[i];
@@ -68,6 +65,7 @@ static int vfprintf(char* string, char const* fmt, va_list arg)
 {
         char c;
         char* tmp_string;
+        char tmp_number[65];
         int length = 0;
 
         while ((c = *fmt++)) {
@@ -84,8 +82,29 @@ static int vfprintf(char* string, char const* fmt, va_list arg)
                         case 's':
                                 tmp_string = va_arg(arg, char*);
                                 memcpy(string + length, tmp_string,
-                                        strlen(tmp_string));
+                                       strlen(tmp_string));
                                 length += strlen(tmp_string);
+                                break;
+                        case 'd':
+                        case 'u':
+                        case 'x':
+                        case 'o':
+                                if (c == 'u') {
+                                        unum_to_string(va_arg(arg, int), 
+                                                       10, tmp_number);
+                                } else if (c == 'd') {
+                                        snum_to_string(va_arg(arg, int),
+                                                       10, tmp_number);
+                                } else if (c == 'x') {
+                                        snum_to_string(va_arg(arg, int),
+                                                       16, tmp_number);
+                                } else if (c == 'o') {
+                                        snum_to_string(va_arg(arg, int),
+                                                       8, tmp_number);
+                                }
+                                memcpy(string + length, tmp_number,
+                                       strlen(tmp_number));
+                                length += strlen(tmp_number);
                                 break;
                         }
                 } else {
