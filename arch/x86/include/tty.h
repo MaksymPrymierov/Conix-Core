c40f21b7bc30edca98da1616b5a034e01fafbc71
Prymierov Maksym <primerovmax@gmail.com>
2020-01-29 14:22:59 +0200

    arch: x86: update scrolling for the tty
    Update scrolling for the tty, resize text buffer from 2000 symbols to
    10000 symbols.
    
    Signed-off-by: Prymierov Maksym <primerovmax@gmail.com>

--- arch/x86/include/tty.h
+++ arch/x86/include/tty.h
@@ -20,7 +20,8 @@ private:
 public:
         static const constexpr u16 height = 25;
         static const constexpr u16 width = 80;
-        static const constexpr u16 capacity = 2000;
+        static const constexpr u16 size_of_text = height * width;
+        static const constexpr u16 capacity = 10000;
         static const constexpr u16 last_line = 1920;
         static const constexpr u16 color = 0x0700;
         static const constexpr u32 memory_address = 0xb8000;
--- arch/x86/tty.cpp
+++ arch/x86/tty.cpp
@@ -2,7 +2,8 @@
 #include <kernel/kernel_lib.h>
 
 static size_t cursor = 0;
-static u16 buffer[2000];
+static size_t offset = 0;
+static u16 buffer[10000];
 
 tty::tty()
 {
@@ -11,18 +12,18 @@ tty::tty()
 
 void tty::clear()
 {
-	for (size_t i = 0; i < capacity; ++i) {
+        for (size_t i = 0; i < capacity; ++i) {
                 buffer[i] = 0;
-	}
+        }
 
         update();
 }
 
 void tty::update()
 {
-        for (size_t i = 0; i < capacity; ++i) {
-                memory[i] = buffer[i];
-	}
+        for (size_t i = offset; i < capacity; ++i) {
+                memory[i - offset] = buffer[i];
+        }
 }
 
 void tty::scroll_down()
@@ -37,18 +38,21 @@ void tty::print_string(const char *string)
         for (size_t i = 0; i < strlen(string); ++i) {
                 switch (string[i]) {
                 case '\n':
-			cursor = (cursor / width + 1) * width;
+                        cursor = (cursor / width + 1) * width;
                         break;
-		case '\b':
-			--cursor;
-			buffer[cursor] = ' ';
-			break;
+                case '\b':
+                        --cursor;
+                        buffer[cursor] = ' ';
+                        break;
                 case '\t':
                         print_string("    ");
                         break;
                 default:
                         if (cursor >= capacity) {
                                 scroll_down();
+                        }
+                        if (cursor >= size_of_text) {
+                                offset = ((cursor - size_of_text) / width + 1) * width;
                         }
                         buffer[cursor] = color + string[i];
                         ++cursor;
