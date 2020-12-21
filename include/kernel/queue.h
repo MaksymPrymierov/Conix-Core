9b31aeda7f091630a6a43bb7d646f6798b7fb5c6
Maksym Prymierov <primerovmax@gmail.com>
2020-12-21 14:07:43 +0200

    kernel: add queue template conteiner
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

--- include/kernel/queue.h
+++ include/kernel/queue.h
@@ -1,25 +1,51 @@
-#ifndef QUEUE
-#define QUEUE
-
+#pragma once
+#include <kernel/list.h>
 #include <kernel/types.h>
 
-struct queue_char_item
+namespace conix {
+namespace kernel {
+namespace std {
+
+template <typename T>
+class queue
 {
-        struct queue_char_item* last_item;
-        char data;
+private:
+        list<T> q;
+
+public:
+
+        queue()
+        {  }
+
+        ~queue() = default;
+
+        T& front() {
+                return q[q.size() - 1];
+        }
+
+        T& back() {
+                return q[0];
+        }
+
+        bool empty() const {
+                return !q.size();
+        }
+
+        size_t size() const {
+                return q.size();
+        }
+
+        void push(const T &data)
+        {
+                q.append(data);
+        }
+
+        void pop()
+        {
+                q.remove(q.end());
+        }
 };
 
-struct queue_char
-{
-        struct queue_char_item* first_item;
-        struct queue_char_item* end_item;
-        size_t size;
-};
-
-extern void queue_char_init(struct queue_char* q);
-extern int queue_char_push(struct queue_char* q, char data);
-extern void queue_char_pop(struct queue_char* q);
-extern char queue_char_show(struct queue_char* q);
-extern void queue_char_clear(struct queue_char* q);
-
-#endif
+}; // std
+}; // kernel
+}; // conix
