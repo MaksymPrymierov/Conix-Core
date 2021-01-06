4b4a75b17de465ad06d334baf7a1883c6a6c8c46
Maksym Prymierov <primerovmax@gmail.com>
2021-01-06 20:25:57 +0200

    arch: x86: kernel: add starting tests via the keyboard
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

--- arch/x86/kernel.cpp
+++ arch/x86/kernel.cpp
@@ -40,46 +40,27 @@ int main(void* heap_top, void* heap_bottom)
         conix::kernel::tests::test_binary_tree test_bt;
         conix::kernel::tests::test_map test_m;
 
-        test_v.start();
-        test_s.start();
-        test_l.start();
-        test_q.start();
-        test_bt.start();
-        test_m.start();
-
-        size_t key_code;
-        char symbol[2] = {'\0'};
-        bool symbol_mode = false;
-        conix::kernel::arch::x86::keyboard k;
-        k.enable_int();
+        conix::kernel::std::string s;
 
         while (1) {
-                if (symbol_mode) {
-                        if (!k.empty()) {
-                                symbol[0] = k.get_key();
+                log << "Enter the test name to start: ";
+                log >> s;
 
-                                if (symbol[0] == '`') {
-                                        log << "\nsymbol mode has been disabled\n";
-                                        symbol_mode = false;
-                                        continue;
-                                }
-
-                                log << symbol;
-                        }
+                if (s == "vector") {
+                        test_v.start();
+                } else if (s == "string") {
+                        test_s.start();
+                } else if (s == "list") {
+                        test_l.start();
+                } else if (s == "queue") {
+                        test_q.start();
+                } else if (s == "binary_tree") {
+                        test_bt.start();
+                } else if (s == "map") {
+                        test_m.start();
                 } else {
-                        if (!k.empty()) {
-                                key_code = k.get_key_number();
-
-                                if (key_code == 1) {
-                                        log << "\nsymbol mode has been enabled\n";
-                                        symbol_mode = true;
-                                        continue;
-                                }
-
-                                log << key_code << "\n";
-                        }
+                        log << "Test not found\n";
                 }
         }
-
         return 0;
 }
