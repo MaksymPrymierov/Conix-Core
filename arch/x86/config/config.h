529072702c21c86975d97be92806f236460209a9
Maksym Prymierov <primerovmax@gmail.com>
2021-01-10 21:35:58 +0200

    Add kernel config files
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

---
+++ arch/x86/config/config.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/* -*- ConixCore -*- ------------------------------------------------------- *
+ *
+ *   Copyright (C) 2021 Maksym Prymierov
+ *
+ * ------------------------------------------------------------------------- */
+
+#pragma once
+
+#include <config/main_config.h>
+
+#define ARCH "x86"
---
+++ include/config/main_config.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/* -*- ConixCore -*- ------------------------------------------------------- *
+ *
+ *   Copyright (C) 2021 Maksym Prymierov
+ *
+ * ------------------------------------------------------------------------- */
+
+#pragma once
+
+#define KERNEL_NAME 	"Conix Core"
+#define KERNEL_LICENSE 	"GPLv3"
+#define KERNEL_VERSION 	"0.0.2"
